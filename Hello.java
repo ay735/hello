7598b906bbb70ca9a5a0d67aa92771c3e439e94c
ay735 <ranjanayush011@gmail.com>
2021-05-10 10:48:59 +0530

    v3

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, World!, this is my first program"); 
-	System.out.println("Hello, World!, this is my second program")
+	System.out.println("Hello, World!, this is my second program");
+	System.out.println("Hello, World!, this is my third program");
     }
 }
